437dcc53b05ce57815b9e1695fe80e3984e14931
Daniel Mellado <dmellado@redhat.com>
2018-01-29 10:31:08 +0000

    Add a devstack plugin for Kuryr Tempest Plugin
    This commit adds a devstack plugin for the kuryr tempest plugin so users
    can directly refer that on their local.conf file.
    
    Change-Id: I818fd1760e20e1ece1d96547a55651a0186f79c5

---
+++ devstack/plugin.sh
@@ -0,0 +1,9 @@
+# install tempest plugin
+function install_kuryr_tempest_plugin {
+    setup_dev_lib "kuryr-tempest-plugin"
+}
+
+if [[ "$1" == "stack" && "$2" == "install"]]; then
+        echo_summary "Installing Kuryr Tempest Plugin"
+        install_kuryr_tempest_plugin
+fi
