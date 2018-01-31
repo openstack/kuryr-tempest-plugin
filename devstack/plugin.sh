55fec4403c75522bd7c2b29c983f897b025907ca
Daniel Mellado <dmellado@redhat.com>
2018-01-31 08:52:46 +0000

    Fix missing blank space in plugin conditional
    There was a missing blank space in the plugin conditional that made bash
    misinterpreter it.
    
    Closes-Bug: #1746456
    Change-Id: Ib81b4ccb0a7f26e3f83beff68d8d041619c0585e

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,7 +3,7 @@ function install_kuryr_tempest_plugin {
     setup_dev_lib "kuryr-tempest-plugin"
 }
 
-if [[ "$1" == "stack" && "$2" == "install"]]; then
+if [[ "$1" == "stack" && "$2" == "install" ]]; then
         echo_summary "Installing Kuryr Tempest Plugin"
         install_kuryr_tempest_plugin
 fi
