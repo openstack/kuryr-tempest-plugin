f07e37c1eb657f92abb46fdc1fc2beeb875a936d
Genadi Chereshnya <gcheresh@redhat.com>
2018-01-31 12:30:32 +0200

    Testing service creation and connectivity
    Creating functions to create delete and get ip from the service
    Updating creation of pod to have a label
    Testing connectivity to the service
    
    Change-Id: I3c6d5dc9614820b889ce10f0ea9c6a6a3f09968f

--- kuryr_tempest_plugin/tests/scenario/base.py
+++ kuryr_tempest_plugin/tests/scenario/base.py
@@ -14,15 +14,21 @@
 
 import time
 
+from oslo_log import log as logging
+
+import requests
+
 from kubernetes import client as k8s_client
 from kubernetes import config as k8s_config
 from kubernetes.stream import stream
 
 from tempest import config
 from tempest.lib.common.utils import data_utils
+from tempest.lib import exceptions as lib_exc
 from tempest.scenario import manager
 
 CONF = config.CONF
+LOG = logging.getLogger(__name__)
 
 
 class BaseKuryrScenarioTest(manager.NetworkScenarioTest):
@@ -52,11 +58,12 @@ class BaseKuryrScenarioTest(manager.NetworkScenarioTest):
             cls.os_admin.floating_ips_client.delete_floatingip(
                 fip['floatingip']['id'])
 
-    def create_pod(self, name=None, image='kuryr/demo',
+    def create_pod(self, name=None, labels=None, image='kuryr/demo',
                    namespace="default"):
-        name = data_utils.rand_name(prefix='kuryr-pod')
+        if not name:
+            name = data_utils.rand_name(prefix='kuryr-pod')
         pod = self.k8s_client.V1Pod()
-        pod.metadata = self.k8s_client.V1ObjectMeta(name=name)
+        pod.metadata = self.k8s_client.V1ObjectMeta(name=name, labels=labels)
 
         container = self.k8s_client.V1Container(name=name)
         container.image = image
@@ -137,3 +144,61 @@ class BaseKuryrScenarioTest(manager.NetworkScenarioTest):
         for project in projects_list['projects']:
             if project_name == project['name']:
                 return project['id']
+
+    def create_service(self, pod_label, service_name=None, api_version="v1",
+                       kind=None, protocol="TCP", port=80, target_port=8080,
+                       spec_type='ClusterIP', namespace="default"):
+        if not service_name:
+            service_name = data_utils.rand_name(prefix='kuryr-service')
+        service = self.k8s_client.V1Service()
+        service.api_version = api_version
+        service.kind = kind
+        service.metadata = self.k8s_client.V1ObjectMeta(name=service_name)
+
+        spec = self.k8s_client.V1ServiceSpec()
+        spec.ports = [self.k8s_client.V1ServicePort(
+            protocol=protocol,
+            port=port,
+            target_port=target_port)]
+        spec.selector = pod_label
+        spec.type = spec_type
+
+        service.spec = spec
+        service_obj = self.k8s_client.CoreV1Api().create_namespaced_service(
+            namespace=namespace, body=service)
+        self.addCleanup(self.delete_service, service_name)
+        return service_name, service_obj
+
+    def delete_service(self, service_name, namespace="default"):
+        self.k8s_client.CoreV1Api().delete_namespaced_service(
+            name=service_name,
+            namespace=namespace)
+
+    def get_service_ip(
+            self, service_name, spec_type="ClusterIP", namespace="default"):
+        api = self.k8s_client.CoreV1Api()
+        while True:
+            time.sleep(5)
+            service = api.read_namespaced_service(service_name, namespace)
+            if spec_type == "LoadBalancer":
+                if service.status.load_balancer.ingress:
+                    return service.status.load_balancer.ingress[0].ip
+            elif spec_type == "ClusterIP":
+                return service.spec.cluster_ip
+            else:
+                raise lib_exc.NotImplemented()
+
+    def wait_service_status(self, service_ip, timeout_period):
+        session = requests.Session()
+        start = time.time()
+        while time.time() - start < timeout_period:
+            try:
+                session.get("http://{0}".format(service_ip), timeout=2)
+                time.sleep(1)
+                return
+            except Exception:
+                LOG.warning('No initial traffic is passing through.')
+                time.sleep(1)
+        LOG.error(
+            "Traffic didn't pass within the period of %s" % timeout_period)
+        raise lib_exc.ServerFault()
