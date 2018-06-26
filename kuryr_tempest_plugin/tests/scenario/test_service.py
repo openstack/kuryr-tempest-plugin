7c296759d9be84e94bb2ff2ae90b057df3414306
Daniel Mellado <dmellado@redhat.com>
2018-06-26 22:25:45 +0200

    Disable load balancer service scenario
    This commits disables also TestLoadBalancerServiceScenario class so it
    won't run where enable_service_tests is disabled. Otherwise it lasts
    test fails due to its resource setup loading.
    
    As it happened with [1] this commit will be reverted as soon as we
    identify the root cause for the issue.
    
    [1] https://review.openstack.org/#/c/577821/
    
    Change-Id: I1b05fba7fcb7a282232544810a1a8b6c7b539438
    Partial-Bug: #1778483

--- kuryr_tempest_plugin/tests/scenario/test_service.py
+++ kuryr_tempest_plugin/tests/scenario/test_service.py
@@ -81,6 +81,8 @@ class TestLoadBalancerServiceScenario(base.BaseKuryrScenarioTest):
         super(TestLoadBalancerServiceScenario, cls).skip_checks()
         if not CONF.network_feature_enabled.floating_ips:
             raise cls.skipException("Floating ips are not available")
+        if not CONF.kuryr_kubernetes.service_tests_enabled:
+            raise cls.skipException("Service tests are not enabled")
 
     @classmethod
     def resource_setup(cls):
